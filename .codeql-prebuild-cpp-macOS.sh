92a0c95e4fde65d296735c4d684929b27ad84878
ReenigneArcher <42013603+ReenigneArcher@users.noreply.github.com>
2024-04-27 11:58:06 -0400

    ci(codeql): fix codeql builds

--- .codeql-prebuild-cpp-macOS.sh
+++ .codeql-prebuild-cpp-macOS.sh
@@ -1,8 +1,24 @@
 # install dependencies for C++ analysis
 set -e
 
+# The following error occurs without arch -arm64 on macOS-14 runner:
+# Error: Cannot install under Rosetta 2 in ARM default prefix (/opt/homebrew)!
+# To rerun under ARM use:
+#     arch -arm64 brew install ...
+
+# detect if this is arm64
+#echo "uname -m: $(uname -m)"
+#echo "arch: $(arch)"
+#echo "uname -p: $(uname -p)"
+#echo "uname -a: $(uname -a)"
+#if [[ "$(uname -m)" == "arm64" ]]; then
+#  brew_cmd="arch -arm64 brew"
+#else
+#  brew_cmd="brew"
+#fi
+
 # install dependencies
-brew install \
+arch -arm64 brew install \
   boost \
   cmake \
   miniupnpc \
